4dd9f6505eaaee5120211a3d1005d118871c79e3
Th3R3ality <oliver.tscherning@gmail.com>
2022-10-31 19:50:57 +0100

    first command works

--- wise mystical tree.js
+++ wise mystical tree.js
@@ -1,12 +1,11 @@
 const net = require("net");
-const { Client, Events, GatewayIntentBits, ActivityType, ApplicationCommandOptionType } = require('discord.js');
-const { token } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Events, GatewayIntentBits, ActivityType, ApplicationCommandOptionType, Collection } = require('discord.js');
+const { token, guildid, channelid, botid } = require("./config.json");
 
 var channel = null;
 var guild = null;
-const guildid = '903229026972352513';
-const channelid = '903229026972352516';
-const botid = '1035937419817652316';
 
 const client = new Client({ 
     intents: [
@@ -15,9 +14,27 @@ const client = new Client({
     ] 
 });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
 /*
 client.on("messageCreate", (message) => {
     if (message.author.id != botid){
@@ -28,29 +45,10 @@ client.on("messageCreate", (message) => {
     }
 });
 */
+
 client.on('ready', () => {
     client.channels.fetch(channelid).then(c => channel = c);
     client.guilds.fetch(guildid).then(g => guild = g);
-    let commands;
-
-    if (guild) {
-        commands = guild.commands;
-    } else {
-        commands = client.application?.commands;
-    }
-
-    commands?.create({
-        name: "send",
-        description: "send a message in the minecraft chat",
-        options: [
-            {
-                name: "message",
-                description: "the message you want to send",
-                required: true,
-                type: ApplicationCommandOptionType.String,
-            }
-        ]
-    });
 
     client.user.setPresence({
         activities: [{name: "the bits and the bytes", type: ActivityType.Watching}],
@@ -59,18 +57,23 @@ client.on('ready', () => {
 });
 client.login(token);
 
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand())
-        return;
-    
-        const { commandName, options } = interaction;
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
 
-        if (commandName === "send") {
-            interaction.reply({
-                content: options,
-                ephemeral: true,
-            });
-        }
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`no command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (e) {
+        console.error(e);
+        await interaction.reply({content: "There was an error while executing this command!", ephemeral: true});
+    }
 });
 
 const server = net.createServer();
@@ -102,14 +105,11 @@ function handleConnection(conn){
                 pres = {
                     activities: [{name: "the bits and the bytes", type: ActivityType.Watching}],
                     status: "dnd"
-                };
-            }
-            else {
+                };} else {
                 pres = {
                     activities: [{name: str + (str == 1 ? " player" : " players"), type: ActivityType.Watching}],
                     status: "online"
-                };
-            }
+                };}
             client.user.setPresence(pres);
         }
         conn.write(d);
