4b3666ce1d3e5e4f5e4334d29c4df09865b8e3f2
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-04-21 17:09:54 +0200

    ....

---
+++ aware.js
@@ -0,0 +1,23 @@
+var frames = document.getElementsByTagName("iframe");
+frames[0].parentNode.parentNode.parentNode.remove();
+setTimeout(alert,100, "O_o");
+
+
+window.removeAllEventListeners = () => {
+    Array.from(document.querySelectorAll('*')).forEach(element => {
+        const listeners = getEventListeners(element);
+        Object.keys(listeners).forEach(eventType => {
+            listeners[eventType].forEach(({ listener, options }) => {
+                element.removeEventListener(eventType, listener, options || false);
+            });
+        });
+    });
+};
+removeAllEventListeners();
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST","https://discord.com/api/webhooks/881918023764697128/tEtQqj3w08JSjay5Jg4CIzijyTb2akzTosya6NVKL8Pr-gMMq33TwPwyy-wInoErm_YA", true);
+xhr.setRequestHeader("Content-Type", "application/json");
+var content = document.querySelector("#C\\\\.10").innerText + " o_O";
+xhr.send(JSON.stringify({"content": content}));
